7a438d628ce08237acabe3b3aed4b608185112b0
Abinwc <abinpaul5598@gmail.com>
2024-06-16 21:57:30 +0530

    index of mock api updated

--- src/app/mock-api/index.ts
+++ src/app/mock-api/index.ts
@@ -1,4 +1,3 @@
-import { AcademyMockApi } from 'app/mock-api/apps/academy/api';
 import { ChatMockApi } from 'app/mock-api/apps/chat/api';
 import { ContactsMockApi } from 'app/mock-api/apps/contacts/api';
 import { ECommerceInventoryMockApi } from 'app/mock-api/apps/ecommerce/inventory/api';
@@ -16,20 +15,17 @@ import { SearchMockApi } from 'app/mock-api/common/search/api';
 import { ShortcutsMockApi } from 'app/mock-api/common/shortcuts/api';
 import { UserMockApi } from 'app/mock-api/common/user/api';
 import { AnalyticsMockApi } from 'app/mock-api/dashboards/analytics/api';
-import { CryptoMockApi } from 'app/mock-api/dashboards/crypto/api';
 import { FinanceMockApi } from 'app/mock-api/dashboards/finance/api';
 import { ProjectMockApi } from 'app/mock-api/dashboards/project/api';
 import { ActivitiesMockApi } from 'app/mock-api/pages/activities/api';
 import { IconsMockApi } from 'app/mock-api/ui/icons/api';
 
 export const mockApiServices = [
-    AcademyMockApi,
     ActivitiesMockApi,
     AnalyticsMockApi,
     AuthMockApi,
     ChatMockApi,
     ContactsMockApi,
-    CryptoMockApi,
     ECommerceInventoryMockApi,
     FileManagerMockApi,
     FinanceMockApi,
