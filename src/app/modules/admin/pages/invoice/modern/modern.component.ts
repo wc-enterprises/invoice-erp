f3f2b03f57cf3ac549284cdcac591883a0929f51
Abinwc <abinpaul5598@gmail.com>
2024-06-21 11:23:38 +0530

    invoice form builder started

--- src/app/modules/admin/pages/invoice/modern/modern.component.ts
+++ src/app/modules/admin/pages/invoice/modern/modern.component.ts
@@ -1,5 +1,6 @@
 import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector       : 'modern',
@@ -12,10 +13,32 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 export class ModernComponent
 {
     editMode: boolean = false;
+    invoiceForm: UntypedFormGroup;
     /**
      * Constructor
      */
-    constructor()
+    constructor(private _formBuilder: UntypedFormBuilder,)
     {
     }
+
+   // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void{
+        this.invoiceForm = this._formBuilder.group({
+            id: [''],
+            billTo: this._formBuilder.group({
+                name: ['', [Validators.required]],
+                address     : [null],
+                phoneNumber : [''],
+                email     : [''],
+            }),
+            carInfo: this._formBuilder.group({})
+        })
+    }
+
 }
