b2f02cc57ff8cee1186b3bf07705bffc99e2ce20
Arun <arunjothi2599@gmail.com>
2024-06-26 15:46:18 +0530

    Invoice form static page done

---
+++ src/app/modules/admin/pages/invoice/invoice-form/invoice-form.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { InvoiceFormComponent } from './invoice-form';
+export default [
+    {
+        path     : '',
+        component: InvoiceFormComponent,
+    },
+] as Routes;
